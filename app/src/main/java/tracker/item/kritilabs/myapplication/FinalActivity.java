b77e073848ecef7aa88e9f374478eede728d11de
Pranesh <pranesh.g@kritilabs.com>
2018-12-12 18:44:33 +0530

    TODO F1

--- app/src/main/java/tracker/item/kritilabs/myapplication/FinalActivity.java
+++ app/src/main/java/tracker/item/kritilabs/myapplication/FinalActivity.java
@@ -8,5 +8,6 @@ public class FinalActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //TODO
     }
 }
