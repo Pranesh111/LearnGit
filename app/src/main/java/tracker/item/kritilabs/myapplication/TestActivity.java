27e42a5e1df9327ba817bde41646d22fe3da2061
Pranesh <pranesh.g@kritilabs.com>
2018-12-12 17:50:59 +0530

    Added Activity creation in Feature 2

---
+++ app/src/main/java/tracker/item/kritilabs/myapplication/TestActivity.java
@@ -0,0 +1,12 @@
+package tracker.item.kritilabs.myapplication;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
