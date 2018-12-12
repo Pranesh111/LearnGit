7887aafca70cec951e9954508407cc261e644e06
Pranesh <pranesh.g@kritilabs.com>
2018-12-12 18:09:17 +0530

    Feature2 Menu Option

---
+++ app/src/main/java/tracker/item/kritilabs/myapplication/FinalActivity.java
@@ -0,0 +1,11 @@
+package tracker.item.kritilabs.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+
+public class FinalActivity extends AppCompatActivity {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+}
